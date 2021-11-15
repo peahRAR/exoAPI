003728b36391962c2d2cb44cd03c51af814a4d61
Pierre-Alexis DECALLONNE <padecallonne@gmail.com>
2021-11-15 22:02:03 +0100

    fin implementation methode CRUD

--- exoAPI/secondTry/Controllers/UtilisateurController.cs
+++ exoAPI/secondTry/Controllers/UtilisateurController.cs
@@ -38,20 +38,39 @@ namespace API.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public List<Utilisateur> Post([FromBody] Utilisateur utilisateur)
         {
+            utilisateurs.Add(utilisateur);
+            return utilisateurs;
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public List<Utilisateur> Put(int id, [FromBody] Utilisateur utilisateur)
         {
+            Utilisateur UserToUpdate = utilisateurs.Find(f => f.Id == id);
+            int index = utilisateurs.IndexOf(UserToUpdate);
+
+            utilisateurs[index].Civilitie = utilisateur.Civilitie;
+            utilisateurs[index].Name = utilisateur.Name;
+            utilisateurs[index].Firstame = utilisateur.Firstame;
+            utilisateurs[index].SecuNb = utilisateur.SecuNb;
+            utilisateurs[index].Adress.StreetNb = utilisateur.Adress.StreetNb;
+            utilisateurs[index].Adress.StreetName = utilisateur.Adress.StreetName;
+            utilisateurs[index].Adress.CityName = utilisateur.Adress.CityName;
+            utilisateurs[index].Adress.CP = utilisateur.Adress.CP;
+
+            return utilisateurs;
+            
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public List<Utilisateur> Delete(int id)
         {
+            Utilisateur utilisateur = utilisateurs.Find(f => f.Id == id);
+            utilisateurs.Remove(utilisateur);
+            return utilisateurs;
         }
     }
 }
